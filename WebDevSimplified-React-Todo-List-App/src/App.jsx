89754c514ccf7f94e62a6587cd6b85092f90060d
C0mradeAndThePhantomMenace <thezerocoolpro@tutamail.com>
2024-08-04 17:20:23 +0000

    Add App Form

--- WebDevSimplified-React-Todo-List-App/src/App.jsx
+++ WebDevSimplified-React-Todo-List-App/src/App.jsx
@@ -1,6 +1,47 @@
+import './App.css'
+
 const App = () => (
     <>
-        <h1>WebDevSimplified-React-Todo-List-App</h1>
+        <header>
+            <h1>WebDevSimplified-React-Todo-List-App</h1>
+        </header>
+        <main>
+            <form action="" className="new-item-form">
+                <div className="form-row">
+                    <label htmlFor="item" >New Item</label>
+                    <input type="text" id='item' />
+                    <button className='btn'>Add</button>
+                </div>
+            </form>
+            <div>
+                <h2 className="header">Todo List</h2>
+                <ul className='list'>
+                <li>
+                        <label >
+                            <input type="checkbox" name="" id="" />
+                            Item 1
+                        </label>
+                        <button className='btn btn-danger'>Delete</button>
+                    </li>
+                    <li>
+                        <label >
+                            <input type="checkbox" name="" id="" />
+                            Item 1
+                        </label>
+                        <button className='btn btn-danger'>Delete</button>
+                    </li>
+                    <li>
+                        <label >
+                            <input type="checkbox" name="" id="" />
+                            Item 1
+                        </label>
+                        <button className='btn btn-danger'>Delete</button>
+                    </li>
+                </ul>
+
+                <label ></label>
+            </div>
+        </main>
     </>
 )
 
